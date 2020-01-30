3ed2edceccc19a0788716fe178242608d732d6fa
yosukedoke <yd.niku@gmail.com>
2020-01-30 13:51:40 +0900

    2-4の開始ファイルを追加

---
+++ src/step2-4/components/TodosProgress.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="todos-progress">
+    <div>{{ displayProgress }}</div>
+    <div class="todos-progress__bar" :style="progressWidth"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "TodosProgress",
+  props: {
+    numDone: Number,
+    numTotal: Number,
+  },
+  computed: {
+    percentOfDone() {
+      return Math.round((this.numDone / this.numTotal) * 100);
+    },
+    displayProgress() {
+      return `Progress: ${this.numDone}/${this.numTotal} (${
+        this.percentOfDone
+      }%)`;
+    },
+    progressWidth() {
+      return `width: ${this.percentOfDone}%`;
+    }
+  }
+};
+</script>
+<style scoped>
+.todos-progress {
+  background: #f0f0f0;
+  padding: 8px 12px;
+  font-size: 13px;
+  line-height: 24px;
+
+  position: relative;
+  z-index: 0;
+}
+.todos-progress__bar {
+  content: "";
+  display: block;
+  background: #fc9;
+
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: -1;
+}
+</style>
